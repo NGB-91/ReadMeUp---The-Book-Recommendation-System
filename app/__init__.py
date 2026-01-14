a354acf6643a0bb2ee19478b9d7e1aec526d1701
Noemí Gómez Bouzada <noemibouzada@gmail.com>
2026-01-14 15:47:32 +0000

    Serve static files explicitly for HF

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        static_folder="static",
+        static_url_path="/static"
+    )
 
-    from app.main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    from app.main.routes import main
+    app.register_blueprint(main)
 
     return app
